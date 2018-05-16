dbcc63dce533c7b19c3f4ed143f49391e45d0bbe
anderson <anderson@java-devops>
2018-05-16 10:31:38 -0400

    Started the modification of the classes to use JPA and a derby database
    updated the domain class employee and the DAO class
    EmployeeDataAccessProductionVersion

--- EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/dataaccess/EmployeeDataAccessProductionVersion.java
+++ EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/dataaccess/EmployeeDataAccessProductionVersion.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.virtualpairprogrammers.employeemanagement.domain.Employee;
 
@@ -12,6 +15,12 @@ import com.virtualpairprogrammers.employeemanagement.domain.Employee;
 @Default
 @ProductionDao
 public class EmployeeDataAccessProductionVersion implements EmployeeDataAccess {
+	
+	// By making the entity manager an attribute of the class with this annotation the application server, 
+	// which is wildfly in this case, will have access to it and will automatically create and handle the 
+	// entity manager and the transaction.
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public void insert(Employee newEmployee) {
@@ -22,13 +31,8 @@ public class EmployeeDataAccessProductionVersion implements EmployeeDataAccess {
 	@Override
 	public List<Employee> findAll() {
 		
-		Employee e1 = new Employee("Kelly", "Blue", "Author", 1900);
-		Employee e2 = new Employee("David", "Brown", "Cleaner", 1600);
-		
-		List<Employee> employees = new ArrayList<>();
-		employees.add(e1);
-		employees.add(e2);
-		
+		Query q = em.createQuery("select employee from Employee employee");
+		List<Employee> employees = q.getResultList();		
 		return employees;
 	}
 
--- EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/domain/Employee.java
+++ EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/domain/Employee.java
@@ -2,14 +2,27 @@ package com.virtualpairprogrammers.employeemanagement.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee implements Serializable {
 	
 	private static final long serialVersionUID = 300668374425845613L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 	private String firstName;
 	private String surname;
 	private String jobRole;
 	private int salary;
+	
+	public Employee() {
+		//Required empty constructor for JPA
+	}
 	
 	public Employee(String firstName, String surname, String jobRole, int salary) {
 		super();
